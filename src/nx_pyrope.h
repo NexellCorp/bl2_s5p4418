12124bca3c1c425c78335d69d9669345b5e1dc61
hans <hans@nexell.co.kr>
2017-02-01 17:43:03 +0900

    nexell: armv7 secure boot bl2
        This patch is boot loading armv7 dispatcher, secure os, and
        non-secure boot loader(u-boot). and run dispatcher with secure os
        and non-secure boot loader startup address.
    
    Change-Id: I714a7cac4a4cc6e401dceeb8358179397ecea178
    Signed-off-by: hans <hans@nexell.co.kr>

---
+++ src/nx_pyrope.h
@@ -0,0 +1,130 @@
+/*
+ * Copyright (C) 2016  Nexell Co., Ltd.
+ * Author: Sangjong, Han <hans@nexell.co.kr>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef NX_ARM_H
+#define NX_ARM_H
+
+#include "cfgBootDefine.h"
+
+//; Standard definitions of mode bits and interrupt (I & F) flags in PSRs
+
+#define Mode_USR	0x10
+#define Mode_FIQ	0x11
+#define Mode_IRQ	0x12
+#define Mode_SVC	0x13
+#define MODE_MON	0x16
+#define Mode_ABT	0x17
+#define Mode_UNDEF	0x1B
+#define Mode_SYS	0x1F
+
+#define A_Bit		(1 << 8)	//; when A bit is set, Abort is disabled
+#define I_Bit		(1 << 7)	//; when I bit is set, IRQ is disabled
+#define F_Bit		(1 << 6)	//; when F bit is set, FIQ is disabled
+
+//;-------------------------------------------------------------------------------
+//; Control register 1 format bit definition
+//;-------------------------------------------------------------------------------
+#define BIT1_XP		(1 <<  23)	//; Subpage AP
+#define BIT1_U		(1 <<  22)	//; Enables unaligned data access
+#define BIT1_L4		(1 <<  15)	//; for ARM4V architecture
+#define BIT1_RR		(1 <<  14)	//; Replacement strategy for ICache & DCache
+#define BIT1_V 		(1 <<  13)	//; Loaction of exception vectors
+#define BIT1_I 		(1 <<  12)	//; ICache enable/disable
+#define BIT1_Z 		(1 <<  11)	//; Branch prediction
+#define BIT1_R 		(1 <<   9)	//; ROM protection
+#define BIT1_S 		(1 <<   8)	//; system protection
+#define BIT1_B 		(1 <<   7)	//; Endianness(Bin/Little : 1/0)
+#define BIT1_C 		(1 <<   2)	//; DCache enable/disable
+#define BIT1_A 		(1 <<   1)	//; Alignment fault enable/disable
+#define BIT1_M 		(1 <<   0)	//; MMU enable/disable
+
+#define PRIMARY_CPU	(0)
+//;==================================================================
+//; Porting defines
+//;==================================================================
+#define L2CC_PL310	0xCF000000
+
+#define L1_COHERENT	0x00015c06	//; Template descriptor for coherent memory (and L2 cache enabled)
+#define L1_NONCOHERENT	0x00000c1e	//; Template descriptor for non-coherent memory
+#define L1_DEVICE	0x00000c06	//; Template descriptor for device memory
+
+#define CPU_CLKSRC	1
+#define BUS_CLKSRC	0
+#define DDR_CLKSRC	0
+#define G3D_CLKSRC	0
+#define MPG_CLKSRC	0
+
+#define USBD_VID	0x2375
+#define USBD_PID	0x4330
+
+#define GPIO_GROUP_A	0
+#define GPIO_GROUP_B	1
+#define GPIO_GROUP_C	2
+#define GPIO_GROUP_D	3
+#define GPIO_GROUP_E	4
+
+#define POLY 0xEDB88320L
+//#define POLY 0x04C11DB7L	//Reverse
+
+#define SUSPEND_SIGNATURE	(0x50575200)	/* PWR (ASCII) */
+#define USBREBOOT_SIGNATURE	(0x85836666)
+
+
+#define SDFSBOOT		2
+#define UARTBOOT		3
+#define SPIBOOT			4
+#define SDBOOT			5
+#define USBBOOT			6
+#define NANDEC			7
+
+
+#define BOOTMODE		12
+
+#define NANDTYPE		0
+#define NANDPAGE		2
+#define SELCS			3
+#define SELSDEX			15
+
+#define eMMCBOOTMODE		7
+#define SDXCPARTITION		9
+#define eMMCBOOT		10
+
+#define UARTBAUD		10
+
+#define SERIALFLASHADDR		9
+
+#define OTG_SESSION_CHECK	6
+
+#define DECRYPT			4
+#define ICACHE			5
+
+
+
+#define INTERNAL_SRAM_SIZE	(32 * 1024)	// 32KB
+#define SECONDBOOT_STACK	0x1000		// 4KB
+
+#define BL2_IMAGE_SIZE		0x10000		// 64KB
+#define BL2_STACK_SIZE		0x10000		// 64KB
+
+#define BASEADDR_SRAM		0xFFFF0000
+#define BASEADDR_NFMEM		0x2C000000
+#define BASEADDR_DDRSDRAM	0x80000000
+#define BASEADDR_SECUREMEM	0xB0000000
+#define SECUREMEM_SIZE		0x01000000	// 16MB
+
+#define DIRECT_IO		0
+#endif
---
+++ src/sysheader.h
@@ -0,0 +1,118 @@
+/*
+ * Copyright (C) 2016  Nexell Co., Ltd.
+ * Author: Sangjong, Han <hans@nexell.co.kr>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef __SYS_HEADER_H__
+#define __SYS_HEADER_H__
+
+#include "cfgBootDefine.h"
+#include "cfgFreqDefine.h"
+
+#include <nx_pyrope.h>
+#include <nx_type.h>
+#include <nx_debug2.h>
+#include <nx_chip.h>
+#include <nx_rstcon.h>
+
+#include <nx_clkpwr.h>
+#include <nx_ecid.h>
+#include <nx_gpio.h>
+#include <nx_alive.h>
+#include <nx_rtc.h>
+#include <nx_tieoff.h>
+#include <nx_intc.h>
+#include <nx_clkgen.h>
+#include <nx_ssp.h>
+#include <nx_uart.h>
+#include <nx_wdt.h>
+
+#include "bl2.h"
+#include "printf.h"
+#include "debug.h"
+#include <common.h>
+
+//------------------------------------------------------------------------------
+//  Set DEBUG Macro
+//------------------------------------------------------------------------------
+
+#if 0 // def NX_DEBUG
+#define SYSMSG printf
+#else
+#define SYSMSG(x, ...)
+#endif
+
+// Memory debug message
+#if 0
+#define MEMMSG  printf
+#else
+#define MEMMSG(x, ...)
+#endif
+
+// UserDebug Message
+#if 0
+#define DBGOUT  printf
+#else
+#define DBGOUT(x, ...)
+#endif
+
+//------------------------------------------------------------------------------
+//  Set global variables
+//------------------------------------------------------------------------------
+
+#if defined(__SET_GLOBAL_VARIABLES)
+
+struct NX_SecondBootInfo *const pSBI =
+    (struct NX_SecondBootInfo * const)BASEADDR_SRAM;
+struct NX_SecondBootInfo *const pTBI =
+    (struct NX_SecondBootInfo * const)BASEADDR_SRAM;
+struct NX_GPIO_RegisterSet (*const pReg_GPIO)[1] =
+    (struct NX_GPIO_RegisterSet (*const)[])PHY_BASEADDR_GPIOA_MODULE;
+struct NX_ALIVE_RegisterSet *const pReg_Alive =
+    (struct NX_ALIVE_RegisterSet * const)PHY_BASEADDR_ALIVE_MODULE;
+struct NX_TIEOFF_RegisterSet *const pReg_Tieoff =
+    (struct NX_TIEOFF_RegisterSet * const)PHY_BASEADDR_TIEOFF_MODULE;
+struct NX_ECID_RegisterSet *const pReg_ECID =
+    (struct NX_ECID_RegisterSet * const)PHY_BASEADDR_ECID_MODULE;
+struct NX_CLKPWR_RegisterSet *const pReg_ClkPwr =
+    (struct NX_CLKPWR_RegisterSet * const)PHY_BASEADDR_CLKPWR_MODULE;
+struct NX_RSTCON_RegisterSet *const pReg_RstCon =
+    (struct NX_RSTCON_RegisterSet * const)PHY_BASEADDR_RSTCON_MODULE;
+struct NX_DREXSDRAM_RegisterSet *const pReg_Drex =
+    (struct NX_DREXSDRAM_RegisterSet * const)PHY_BASEADDR_DREX_MODULE_CH0_APB;
+struct NX_DDRPHY_RegisterSet *const pReg_DDRPHY =
+    (struct NX_DDRPHY_RegisterSet * const)PHY_BASEADDR_DREX_MODULE_CH1_APB;
+struct NX_RTC_RegisterSet *const pReg_RTC =
+    (struct NX_RTC_RegisterSet * const)PHY_BASEADDR_RTC_MODULE;
+
+struct NX_WDT_RegisterSet *const pReg_WDT =
+    (struct NX_WDT_RegisterSet * const)PHY_BASEADDR_WDT_MODULE;
+
+#else
+
+extern struct NX_SecondBootInfo *const pSBI; // second boot info
+extern struct NX_SecondBootInfo *const pTBI; // third boot info
+extern struct NX_GPIO_RegisterSet (*const pReg_GPIO)[1];
+extern struct NX_ALIVE_RegisterSet *const pReg_Alive;
+extern struct NX_TIEOFF_RegisterSet *const pReg_Tieoff;
+extern struct NX_ECID_RegisterSet *const pReg_ECID;
+extern struct NX_CLKPWR_RegisterSet *const pReg_ClkPwr;
+extern struct NX_RSTCON_RegisterSet *const pReg_RstCon;
+extern struct NX_DREXSDRAM_RegisterSet *const pReg_Drex;
+extern struct NX_DDRPHY_RegisterSet *const pReg_DDRPHY;
+extern struct NX_RTC_RegisterSet *const pReg_RTC;
+#endif
+
+#endif //	__SYS_HEADER_H__
