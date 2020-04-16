c23bdbf42ad469d0bcd8a62ccc4c6011b88992b4
GyoungBo Min <mingyoungbo@nexell.co.kr>
2020-04-16 13:26:19 +0900

    nexell: fixed print error
    This patch fixed to do not shown '0' print error.
    
    Signed-off-by: GyoungBo Min <mingyoungbo@nexell.co.kr>
    Change-Id: Iad06864bb84d8f5b8993cb1fb1b737b584c38ce1

--- src/printf.c
+++ src/printf.c
@@ -14,166 +14,195 @@
  * History	: 2017.02.28 new release
  */
 #include <stdarg.h>
-#include "nx_type.h"
-#include "printf.h"
+//#include "nx_type.h"
+#include <printf.h>
 
-
-#define PAD_RIGHT	1
-#define PAD_ZERO	2
-
-static int printstring(const char *string, int width, int pad)
+static void printchar(char **str, int c)
 {
-	register int cc = 0, padchar = ' ';
-	register char ch;
+	extern void putchar(char ch);
+
+	if (str) {
+		**str = c;
+		++(*str);
+	} else
+		(void)putchar((char)c);
+}
+
+#define PAD_RIGHT 1
+#define PAD_ZERO 2
+
+static int prints(char **out, const char *string, int width, int pad)
+{
+	register int pc = 0, padchar = ' ';
 
 	if (width > 0) {
 		register int len = 0;
-		register const char *ptr = string;
-		if (pad & PAD_ZERO)
-			padchar = '0';
-		while (*ptr++)
+		register const char *ptr;
+		for (ptr = string; *ptr; ++ptr)
 			++len;
 		if (len >= width)
 			width = 0;
 		else
 			width -= len;
+		if (pad & PAD_ZERO)
+			padchar = '0';
 	}
-
 	if (!(pad & PAD_RIGHT)) {
-		while (width--) {
-			putchar(padchar);
-			++cc;
+		for (; width > 0; --width) {
+			printchar(out, padchar);
+			++pc;
 		}
 	}
-
-	do {
-		ch = *string++;
-		putchar(ch);
-		++cc;
-	} while (ch);
-
+	for (; *string; ++string) {
+		printchar(out, *string);
+		++pc;
+	}
 	for (; width > 0; --width) {
-		putchar(padchar);
-		++cc;
+		printchar(out, padchar);
+		++pc;
 	}
 
-	return cc;
+	return pc;
 }
 
 /* the following should be enough for 32 bit int */
-#define BUF_LEN 12
-static int printnumber(int i, int b, int sg,
-		int width, int pad, int letbase)
+#define PRINT_BUF_LEN 12
+int getquotient(int, int);
+int getremainder(int, int);
+static int printi(char **out, int i, int b, int sg, int width, int pad, int letbase)
 {
-	char buf[BUF_LEN];
+	char print_buf[PRINT_BUF_LEN];
 	register char *s;
-	register int t, neg = 0, cc = 0;
+	register int t, neg = 0, pc = 0;
 	register unsigned int u = i;
 
-	if (i == 0)
-		return printstring("\0", width, pad);
+	if (i == 0) {
+		print_buf[0] = '0';
+		print_buf[1] = '\0';
+		return prints(out, print_buf, width, pad);
+	}
 
 	if (sg && b == 10 && i < 0) {
 		neg = 1;
 		u = -i;
 	}
 
-	s = buf + BUF_LEN - 1;
+	s = print_buf + PRINT_BUF_LEN - 1;
 	*s = '\0';
 
 	while (u) {
-		t = u % b;
+		//		t = u % b;
+		t = getremainder(u, b);
 		if (t >= 10)
 			t += letbase - '0' - 10;
 		*--s = t + '0';
-		u /= b;
+		//		u /= b;
+		u = getquotient(u, b);
 	}
 
 	if (neg) {
 		if (width && (pad & PAD_ZERO)) {
-			putchar('-');
-			++cc;
+			printchar(out, '-');
+			++pc;
 			--width;
-		} else
+		} else {
 			*--s = '-';
+		}
 	}
 
-	return cc + printstring(s, width, pad);
+	return pc + prints(out, s, width, pad);
+}
+
+static int print(char **out, const char *format, va_list args)
+{
+	register int width, pad;
+	register int pc = 0;
+	char scr[2];
+
+	for (; *format != 0; ++format) {
+		if (*format == '%') {
+			++format;
+			width = pad = 0;
+			if (*format == '\0')
+				break;
+			if (*format == '%')
+				goto out;
+			if (*format == '-') {
+				++format;
+				pad = PAD_RIGHT;
+			}
+			while (*format == '0') {
+				++format;
+				pad |= PAD_ZERO;
+			}
+			for (; *format >= '0' && *format <= '9'; ++format) {
+				width *= 10;
+				width += *format - '0';
+			}
+			if (*format == 's') {
+				register char *s = (char *)va_arg(args, int);
+				pc += prints(out, s ? s : "(null)", width, pad);
+				continue;
+			}
+			if( *format == 'd' ) {
+				pc += printi (out, va_arg( args, int ), 10, 1, width, pad, 'a');
+				continue;
+			}
+			if( *format == 'x' ) {
+				pc += printi (out, va_arg( args, int ), 16, 0, width, pad, 'a');
+				continue;
+			}
+			if( *format == 'X' ) {
+				pc += printi (out, va_arg( args, int ), 16, 0, width, pad, 'A');
+				continue;
+			}
+			if( *format == 'u' ) {
+				pc += printi (out, va_arg( args, int ), 10, 0, width, pad, 'a');
+				continue;
+			}
+			if( *format == 'c' ) {
+				/* char are converted to int then pushed on the stack */
+				scr[0] = (char)va_arg( args, int );
+				scr[1] = '\0';
+				pc += prints(out, scr, width, pad);
+				continue;
+			}
+		} else {
+out:
+			printchar(out, *format);
+			++pc;
+		}
+	}
+	if (out)
+		**out = '\0';
+
+	return pc;
 }
 
 int printf(const char *format, ...)
 {
 	va_list args;
-	register int cc = 0;
+	int ret;
 
 	va_start(args, format);
-
-	for (; *format != 0; ++format) {
-		register int width, pad;
-		char ch, chbase;
-
-		ch = *format;
-		if (ch != '%') {
-			putchar(ch);
-			++cc;
-			continue;
-		}
-		ch = *++format;
-		width = pad = 0;
-		if (ch == '\0')
-			break;
-		if (ch == '%') {
-			putchar(ch);
-			++cc;
-		}
-		if (ch == '-') {
-			ch = *++format;
-			pad = PAD_RIGHT;
-		}
-		while (ch == '0') {
-			ch = *++format;
-			pad |= PAD_ZERO;
-		}
-		while (ch >= '0' && ch <= '9') {
-			width *= 10;
-			width += ch - '0';
-			ch = *++format;
-		}
-
-		chbase = (ch & 0xE0) | 1;
-		switch (ch) {
-		case 'd':
-			cc += printnumber(va_arg(args, int),
-					10, 1, width, pad, chbase);
-			continue;
-		case 'u':
-			cc += printnumber(va_arg(args, int),
-					10, 0, width, pad, chbase);
-			continue;
-		case 'x':
-		case 'X':
-			cc += printnumber(va_arg(args, int),
-					16, 0, width, pad, chbase);
-			continue;
-		case 'c': {
-			char scr[2];
-			scr[0] = (char)va_arg(args, int);
-			scr[1] = '\0';
-			cc += printstring(scr, width, pad);
-			continue;
-		}
-		case 's': {
-			register char *s = (char *)va_arg(args, int);
-			cc += printstring(s ? s : "(null)", width, pad);
-			continue;
-		}
-		default:
-			continue;
-		}
-	}
-
+	ret = print(0, format, args);
 	va_end(args);
 
-	return cc;
+	return ret;
 }
 
+int empty_printf(const char *format, ...)
+{
+	format = format;
+	return 0;
+}
+
+#if 0
+int sprintf(char *out, const char *format, ...)
+{
+	va_list args;
+
+	va_start( args, format );
+	return print( &out, format, args );
+}
+#endif
